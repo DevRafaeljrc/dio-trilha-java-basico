ac4b42c4b2e89a823c692b11a2a9ecf611f4d7fc
Devrafaeljrc <Devrafaeljrc@outlook.com>
2025-03-18 22:53:44 -0300

    correção e alinhamento do codigo

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,52 +1,23 @@
 import java.util.Scanner;
-
+//foi usaso o import java.util.Scanner; para que o programa possa receber dados do usuário
 public class ContaTerminal {
-    /**
-     * @param args
-     */
-    public static void main(String[] args)  {
-        //TODO: Conhecer e importar a classe Scanner
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o Número da conta: ");
-        int numero = scanner.nextInt();
+    Scanner scanner = new Scanner(System.in);
+//foi criado um objeto scanner para que o programa possa receber dados do usuário
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite o número da conta: ");
+            String contaNumero = scanner.nextLine();
+            System.out.print("Digite a agência: ");
+            String agencia = scanner.nextLine();
+            System.out.print("Digite o nome do cliente: ");
+            String nome = scanner.nextLine();
+            System.out.print("Digite o saldo da conta: ");
+            double saldo = scanner.nextDouble();
+            scanner.close();
 
-        System.out.println("Agencia: ");
-         int agencia = scanner.nextInt();
-
-        System.out.println("Nome: ");
-        String nome = scanner.next();
-
-        System.out.println("Saldo: ");
-        double saldo = scanner.nextDouble();
-
-        System.out.println("Limite: ");
-        double limite = scanner.nextDouble();
-
-        Conta conta = new Conta(nome, numero, saldo, limite);
-        System.out.println("Conta criada com sucesso!");
-        scanner.close();
+            //Usaso o System.out.println para que o programa possa imprimir na tela as informações do cliente
+            System.out.println("Olá, " + nome + "! " + "obrigado por abrir sua conta na agência " + agencia + " e sua conta é " + contaNumero + ". Seu saldo atual é de R$ " + saldo + ". Já está disponível para saque.");
+        }
     }
+    
 }
-
-
-
-
-
-
-
-
-        
-        
-                
-        
-        
-                
-        
-                //TODO: Exibir mensagem  para o novo usuário
-        
-                //TODO: Obter pela scanner os valores digitado no treminal
-        
-                                //TODO; Exibir a mensagem conta criada
-                    
-                
-        
